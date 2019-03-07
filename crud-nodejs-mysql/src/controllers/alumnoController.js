f90614e7a8b3d715bd5cddf8cc9622f483768965
Gastón Marichal <marichalgaston@gmail.com>
2019-03-06 23:38:11 -0300

    Curso de Node,EJs, MySQL completado

--- crud-nodejs-mysql/src/controllers/alumnoController.js
+++ crud-nodejs-mysql/src/controllers/alumnoController.js
@@ -31,4 +31,49 @@ controller.save = (req, res) => {
     });
 };
 
+controller.delete = (req, res) => {
+    const { id } = req.params;
+    //const id = req.params.id; OLD WAY
+    req.getConnection((err, conn) => {
+        if(err){
+            console.log('Unable to connect to database: ' + err);
+        }
+        conn.query('DELETE FROM alumno WHERE id = ?', [id], (err, row) => {
+            //console.log(row);
+            res.redirect('/');
+        });
+    });
+};
+
+controller.edit = (req, res) => {
+    const { id } = req.params;
+    //const id = req.params.id; OLD WAY
+    req.getConnection((err, conn) => {
+        if(err){
+            console.log('Unable to connect to database: ' + err);
+        }
+        conn.query('SELECT * FROM alumno WHERE id = ?', [id], (err, row) => {
+            //console.log(row);
+            res.render('alumno_edit', {
+                data: row[0]
+            });
+        });
+    });
+};
+
+controller.update = (req, res) => {
+    const { id } = req.params;
+    const alumno = req.body;
+    //const id = req.params.id; OLD WAY
+    req.getConnection((err, conn) => {
+        if(err){
+            console.log('Unable to connect to database: ' + err);
+        }
+        conn.query('UPDATE alumno SET ? WHERE id = ?', [alumno, id], (err, row) => {
+        //console.log(row);
+        res.redirect('/');
+        });
+    });
+};
+
 module.exports = controller;
--- crud-nodejs-mysql/src/routes/alumnos.js
+++ crud-nodejs-mysql/src/routes/alumnos.js
@@ -5,6 +5,10 @@ const alumnoController = require('../controllers/alumnoController');
 
 router.get('/', alumnoController.list);
 router.post('/add', alumnoController.save);
+router.get('/delete/:id', alumnoController.delete);
+router.get('/update/:id', alumnoController.edit);
+router.post('/update/:id', alumnoController.update);
+
 
 
 module.exports = router;
